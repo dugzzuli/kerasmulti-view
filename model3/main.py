0c53eb8339e8d756194a701d63438b93399ecc19
dugking <bingwei2642@qq.com>
2020-02-01 21:53:50 +0800

    更新路径

--- model3/main.py
+++ model3/main.py
@@ -1,9 +1,11 @@
 # coding=utf-8
 
 import numpy as np
-import  scipy.io as sio
-import numpy as np
+import scipy.io as sio
+
 from Util.Util import node_clustering
+
+
 def get_data(path='data/3sources.mat'):
     data=sio.loadmat(path)
     a_sque = np.squeeze(data.get("data"))
